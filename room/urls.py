47ba31c341a2a0fb0e6ff971fbabf09013ca395d
Royweru <royezi17@gmail.com>
2023-07-14 21:14:34 +0300

    modified:   db.sqlite3 	modified:   room/urls.py 	modified:   room/views.py 	modified:   templates/Rooms.html

--- room/urls.py
+++ room/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('room/',views.room_page , name='rooms'),
     path('room/<str:pk>/', views.get_room, name='room'),
 
-    path('create-room/',views.create_room, name='create')
+    path('create-room/',views.create_room, name='create'),
+    path('update-room/<str:pk>/',views.update_room, name='update')
 ]
