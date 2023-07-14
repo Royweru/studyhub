5a8714ba735252c60a48490625f176a91aa0e977
Royweru <royezi17@gmail.com>
2023-07-14 16:29:11 +0300

    new file:   room/forms.py 	modified:   room/urls.py 	modified:   room/views.py 	new file:   templates/Createroom.html 	modified:   templates/Rooms.html 	modified:   templates/home.html 	modified:   templates/navbar.html 	modified:   templates/room.html

--- room/urls.py
+++ room/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from. import views
+
 urlpatterns = [
-    path('',views.room_page),
-    path('<str:pk>/', views.getroom)
+    path('',views.room_page , name='rooms'),
+    # path('<str:pk>/', views.getroom, name='room'),
+    path('create-room/',views.create_room, name='create')
 ]
--- room/views.py
+++ room/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 def room_page(request):
     rooms = Room.objects.all() 
     context = {'rooms':rooms}
     return render(request, 'Rooms.html',context)
 
 
-def getroom(request,pk):
-    rooms = Room.objects.get(id=pk)
-    context = {rooms:'rooms'}
+def get_room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
     return render(request ,'room.html',context )
   
+def create_room(request):
+    form = RoomForm()
+    context = {'form':form}
+    return render(request, 'Createroom.html', context )
