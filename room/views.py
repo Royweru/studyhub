f6e770136bc5f748aabc996f3c12a360059e7d2f
Royweru <royezi17@gmail.com>
2023-07-15 14:57:39 +0300

    modified:   room/views.py 	modified:   templates/Rooms.html 	modified:   templates/navbar.html

--- room/views.py
+++ room/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import Q
 def room_page(request):
-    rooms = Room.objects.all() 
-    context = {'rooms':rooms}
+    q= request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains = q)|
+                                Q(name__icontains=q)|
+                                Q(description__icontains=q)
+                                ) 
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms':rooms ,'topics':topics, 'room_count':room_count}
     return render(request, 'Rooms.html',context)
 
 
 def get_room(request,pk):
     room = Room.objects.get(id=pk)
+    
     context = {'room':room}
     return render(request ,'room.html',context )
   
