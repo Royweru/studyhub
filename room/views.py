b6e529f0a229b68b87f3cafdfd0ccd2bc4a00873
Royweru <royezi17@gmail.com>
2023-07-18 19:01:19 +0300

    modified:   room/views.py 	modified:   db.sqlite3

--- room/views.py
+++ room/views.py
@@ -11,8 +11,8 @@ from django.db.models import Q
 def room_page(request):
     q= request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains = q)|
-                                Q(name__icontains=q)|
-                                Q(description__icontains=q)
+                                Q(name__icontains = q)|
+                                Q(description__icontains = q)
                                 ) 
     topics = Topic.objects.all()
     room_count = rooms.count()
